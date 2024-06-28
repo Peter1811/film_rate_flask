a9450dc6feb663bd87c751d92bca4ea45482ccc9
Peter1811 <78861235+Peter1811@users.noreply.github.com>
2024-06-28 19:13:22 +0300

    changed styles for main menu, added form to add_film page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -7,23 +7,26 @@ main_menu = {'Main page': 'main_page', 'Add film': 'add_film', 'Get the list of 
 
 @app.route('/')
 def main_page():
-    return render_template('main_page.html', main_menu=main_menu)
+    return render_template('main_page.html', main_menu=main_menu, title='Main page')
 
 @app.route('/film/<int:film_id>')
 def film_page(film_id):
-    return render_template('film_page.html', main_menu=main_menu, film_id=film_id)
+    return render_template('film_page.html', main_menu=main_menu, title='Film page', film_id=film_id)
 
-@app.route('/add-film')
+@app.route('/add-film', methods=['POST', 'GET'])
 def add_film():
-    return render_template('add_film.html', main_menu=main_menu)
+    res = render_template('add_film.html', main_menu=main_menu, title='Add film')
+    if request.method == 'POST':
+        print(request.form['film_name'])
+    return res
 
 @app.route('/films-library')
 def get_films():
-    return render_template('films_list.html', main_menu=main_menu)
+    return render_template('films_list.html', main_menu=main_menu, title='List of added films')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', main_menu=main_menu)
+    return render_template('about.html', main_menu=main_menu, title='About site')
 
 @app.errorhandler(404)
 def page_not_found(error):
