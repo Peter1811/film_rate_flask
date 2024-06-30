67af7ee5c7b0c8f0b10ec9f294e79508c46a05f6
Peter1811 <78861235+Peter1811@users.noreply.github.com>
2024-06-30 21:23:12 +0300

    removed config.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, url_for
-import db, views
-
-
+import views
 
 app = Flask(__name__)
 
@@ -11,8 +9,6 @@ app.add_url_rule('/add-film', view_func=views.add_film, methods=['GET', 'POST'])
 app.add_url_rule('/films-library', view_func=views.get_films)
 
 app.register_error_handler(404, views.page_not_found)
-    
-# db.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
