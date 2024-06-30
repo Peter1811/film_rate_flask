d56b19d9db541ba950b727fef5509affc8302ffd
Peter1811 <78861235+Peter1811@users.noreply.github.com>
2024-06-30 21:21:30 +0300

    first version of database connection

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-import sqlite3
-
 from flask import Flask, url_for
-import views
+import db, views
+
+
 
 app = Flask(__name__)
 
@@ -12,23 +11,8 @@ app.add_url_rule('/add-film', view_func=views.add_film, methods=['GET', 'POST'])
 app.add_url_rule('/films-library', view_func=views.get_films)
 
 app.register_error_handler(404, views.page_not_found)
-
-app.config.from_object(__name__)
-app.config.update(dict(DATABASE=os.path.join(app.root_path, 'films.db')))
-
-def connect_db():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    conn_row_factory = sqlite3.Row
-    return conn
-
-def create_db():
-    db = connect_db()
-    with app.open_resource('sq_db.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-
-    db.commit()
-    db.close()
     
+# db.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- views.py
+++ views.py
@@ -1,4 +1,11 @@
-from flask import render_template
+import dotenv
+import os
+import psycopg2
+
+from flask import render_template, request
+
+
+dotenv.load_dotenv('.env')
 
 
 main_menu = {'Main page': 'main_page', 'Add film': 'add_film', 'Get the list of film': 'get_films'}
@@ -13,6 +20,32 @@ def film_page(film_id):
 
 def add_film():
     res = render_template('add_film.html', main_menu=main_menu, title='Add film')
+
+    if request.method == 'POST':
+        print(request.form['film_name'])
+        connection = psycopg2.connect(
+            database=os.getenv('DATABASE'),
+            user=os.getenv('USER'),
+            password=os.getenv('PASSWORD'),
+            host=os.getenv('HOST'),
+            port=os.getenv('PORT')
+        )
+
+        cur = connection.cursor()
+        name = request.form['film_name']
+        director = request.form['film_director']
+        short_description = request.form['description']
+
+        cur.execute(
+            '''INSERT INTO film (film_name, director, short_description) VALUES (%s, %s, %s)''',
+            (name, director, short_description)
+        )
+
+        connection.commit()
+
+        cur.close()
+        connection.close()
+    
     return res
 
 
